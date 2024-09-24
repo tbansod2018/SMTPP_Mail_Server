891c89f9c423650ead502d81eb8fefae69431101
tbansod2018 <tbansod2018@gmail.com>
2024-09-25 01:52:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { SMTPServer } = require('smtp-server');
+
+const server = new SMTPServer({
+    allowInsecureAuth: true,
+    authOptional: true,
+    onConnect(session, cb) {
+        console.log("onConnect");
+        cb(); // Call the callback to accept the connection
+    },
+    onMailFrom(address, session, cb) {
+        console.log(`Mail from: ${address}`);
+        cb(null, address); // Accept the sender address
+    },
+    onRcptTo(address, session, cb) {
+        console.log(`Recipient to: ${address}`);
+        cb(null, address); // Accept the recipient address
+    },
+    onData(stream, session, cb) {
+        stream.on('data', (data) => console.log(`Received data: ${data.toString()}`));
+        stream.on('end', cb); // Call cb when the stream ends
+    }
+});
+
+
+server.listen(587, () => console.log('SMTP server is running on port 587'));
